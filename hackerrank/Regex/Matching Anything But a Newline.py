82209b04143c70c72fce78a06d00f792842eccc5
kaili <26805525+kaili-chen@users.noreply.github.com>
2024-04-13 16:46:19 +0800

    Create Matching Anything But a Newline.py

---
+++ hackerrank/Regex/Matching Anything But a Newline.py
@@ -0,0 +1,16 @@
+# Python 3
+
+### Task
+# You have a test string S.
+# Your task is to write a regular expression that matches only and exactly strings of form: abc.def.ghi.jkx, where each variable a,b,c,d,e,f,g,h,i,j,k,x can be any single character except the newline.
+
+regex_pattern = r"^(.{3}\..{3}\..{3}\.).{3}$"	# Do not delete 'r'.
+
+import re
+import sys
+
+test_string = input()
+
+match = re.match(regex_pattern, test_string) is not None
+
+print(str(match).lower())
