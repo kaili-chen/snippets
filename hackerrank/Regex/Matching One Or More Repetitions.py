4db168e147a826ed04142d6c34cb0c1ecc3ffac5
kaili <26805525+kaili-chen@users.noreply.github.com>
2024-04-14 17:36:21 +0800

    Create Matching One Or More Repetitions.py

---
+++ hackerrank/Regex/Matching One Or More Repetitions.py
@@ -0,0 +1,14 @@
+# Python 3
+
+### Task
+# Write a regex that will match S using the following conditions:
+# - S should begin with 1 or more digits
+# - After that, S should have 1 or more uppercase letters
+# - S should end with 1 or more lowercase letters
+###
+
+Regex_Pattern = r'^\d+[A-Z]+[a-z]+$'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
